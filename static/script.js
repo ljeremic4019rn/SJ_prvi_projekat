5f8801c7d1b67f8a715ae85fd13a2c82fc1948cd
Luka Jeremic <ljeremic4019rn@raf.rs>
2021-12-21 12:35:58 +0100

    dodata front end validacija

--- static/script.js
+++ static/script.js
@@ -168,22 +168,16 @@ function formAdd(elem,formName){
         },
         body: stringifiedData
     })
-    // .then( res => res.json() )
-    // .then(result => {
-    //     console.log('Success:', result);
-    //     updateList(elementType)
-    //   })
-    //   .catch(error => {
-    //     console.error('Error:', error);
-    //   });
+
     .then( res => res.json() )
-    .then( el => {
-        if (el.msg) {
-            alert(el.msg, 'ovo je error msg');
-        } else {
-            updateList(elementType)
-        }
-    });
+    .then( updateList(elementType))
+    // .then( el => {
+    //     if (el.msg) {
+    //         alert(el.msg, 'ovo je error msg');
+    //     } else {
+    //         updateList(elementType)
+    //     }
+    // });
 }
 
 
@@ -226,7 +220,7 @@ function displayFields(data, checker){
 
         let parameters; //parametri iz elementa
         if (checker == 'book'){
-            parameters = `Id: ${element.id}: Name: ${element.name} Writer:${element.writer} Genre: ${element.genre} Publisher: ${element.publisher} `
+            parameters = `Id: ${element.id}: Name: ${element.name} Writer: ${element.writer} Genre: ${element.genre} Publisher: ${element.publisher} `
         }
         else if (checker == 'user'){            
             parameters = `Id: ${element.id}: Credentials:  -${element.name} ${element.lastname}- Username: ${element.username}`
